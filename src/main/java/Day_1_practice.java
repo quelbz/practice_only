a0c756e10d395028c94c49fb20ae61a68a4ac5a9
quelbz <quelbyfernandez18@gmail.com>
2024-12-14 05:19:13 +0800

    Loops Sample

--- src/main/java/Day_1_practice.java
+++ src/main/java/Day_1_practice.java
@@ -1,5 +1,26 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Day_1_practice {
     public static void main(String[] args) {
-        System.out.println("Hello, Quelbz Fernandez!");
+        String [][] names = {
+                {"quelbz","quelbz123"},
+                {"obet","obet123"},
+                {"tantin","tantin123"},
+                {"nek","nek123"},
+                {"akel","akel123"}
+        };
+
+       for (String row[]: names) {
+           for (String info: row) {
+               if (info.equals("tantin")) {
+                   System.out.println(info);
+                   break;
+               }
+               else {
+                   System.out.println("Not Matched");
+               }
+           }
+       }
     }
 }
